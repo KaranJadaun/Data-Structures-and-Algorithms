6726c7e5ba4024b1faf94b551b9f4221a86e2c86
Karan Jadaun <jadaunfamily6@gmail.com>
2023-08-07 09:01:41 +0530

    Create mergeSort.py

---
+++ Sorting/mergeSort.py
@@ -0,0 +1,28 @@
+# approach is to first partition an array till its size go to one 
+# after partitioning it recursively simply merge the left and right array in main arr
+# using three pointers for left, right, and arr respectively.
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+        mergeSort(L)
+        mergeSort(R)
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
