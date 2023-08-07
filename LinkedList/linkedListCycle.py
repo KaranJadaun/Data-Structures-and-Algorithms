23a8efc73bdb42ca513e4d04cfb95ffc4292c2a0
Karan Jadaun <jadaunfamily6@gmail.com>
2023-08-07 09:34:30 +0530

    Update linkedListCycle.py

--- LinkedList/linkedListCycle.py
+++ LinkedList/linkedListCycle.py
@@ -2,6 +2,10 @@
 # it is obvious that if there is a cycle there has to be a point where both pointers meet
 # so we check after each iteration if slow == fast and return directly from there
 
+# to check length of loop make slow = head and then again start its iterations with cnt++ variable
+# if return the cnt variable when they meet again.
+
+
 def hasCycle(self, head: Optional[ListNode]) -> bool:
     slow = head
     fast = head
